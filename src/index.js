f9df14306d58e5ce045dba0c500a5a4b29eff218
jordydehaes <jordy.dehaes@student.odisee.be>
2021-02-12 23:38:12 +0100

    Added Image and OCR support

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { client } from "./client.js";
 import chalk from "chalk";
 import patch from "log-timestamp";
 import Discord from "discord.js";
+import { createWorker } from 'tesseract.js';
 
 dotenv.config();
-
 const discordClient = new Discord.Client();
 
-const twitterAccount = "Jordy_BE_";
+const twitterAccount = "PrismAIO";
 let latestTweet = 0;
 let newTweet = 1;
 let firstTweet = true;
@@ -19,7 +19,7 @@ const startMonitor = () => {
 }
 
 const checkTweets = async () => {
-  console.log(chalk.blueBright("Monitoring... 🔎"));
+  console.log(chalk.blueBright(`Monitoring @${twitterAccount}... 🔎`));
   const data = await client.tweets.statusesUserTimeline({
     screen_name: twitterAccount,
     count: 1,
@@ -27,16 +27,22 @@ const checkTweets = async () => {
     exclude_replies: true,
   });
   newTweet = data;
+
   if (latestTweet != newTweet[0].id) {
     if (firstTweet) {
       firstTweet = false;
       latestTweet = newTweet[0].id;
       return;
     } else {
-      console.log(
-        chalk.bold.rgb(30, 255, 0)(`New tweet (${newTweet[0].id}) found 🤑! Sent to discord. `)
-      );
-      sendEmbed(newTweet);
+      console.log(chalk.bold.rgb(30, 255, 0)(`New tweet (${newTweet[0].id}) found 🤑! Sent to discord. `));
+      if (newTweet[0].hasOwnProperty('extended_entities')) {
+        console.log(chalk.bold.rgb(127, 3, 252)("Image detected..."));
+        sendEmbed(newTweet);
+        sendImageEmbed(newTweet);
+        solveOCR(newTweet);
+      } else {
+        sendEmbed(newTweet);
+      }
       latestTweet = newTweet[0].id;
     }
   } else {
@@ -71,6 +77,51 @@ const sendEmbed = async (tweetData) => {
 
   await channel.send(newTweetEmbed);
 };
+
+const sendImageEmbed = async (tweetData) => {
+  const channel = discordClient.channels.cache.get("809499372307611728");
+
+  const newTweetEmbed = new Discord.MessageEmbed()
+    .setColor("#03bafc")
+    .setImage(tweetData[0].extended_entities.media[0].media_url)
+    .setTimestamp()
+    .setFooter(
+      "Developed by Jordy",
+      "https://cdn.discordapp.com/attachments/568532483772252160/809505822056251392/jorders-3.jpg"
+    );
+
+  await channel.send(newTweetEmbed);
+};
+
+const sendOCREmbed = async (OCR) => {
+  const channel = discordClient.channels.cache.get("809499372307611728");
+
+  const newTweetEmbed = new Discord.MessageEmbed()
+    .setColor("#03bafc")
+    .addField('**OCR**', OCR)
+    .setTimestamp()
+    .setFooter(
+      "Developed by Jordy",
+      "https://cdn.discordapp.com/attachments/568532483772252160/809505822056251392/jorders-3.jpg"
+    );
+
+  console.log(chalk.bold.rgb(227, 3, 252)("OCR solved! Sent to discord."))
+  await channel.send(newTweetEmbed);
+};
+
+const solveOCR = async (tweetData) => {
+  console.log(chalk.bold.rgb(127, 3, 252)("Solving OCR... 🔃"));
+  const worker = createWorker();
+  
+  (async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(tweetData[0].extended_entities.media[0].media_url);
+    sendOCREmbed(text);
+    await worker.terminate();
+  })();
+}
 
 discordClient.login(process.env.BOT_TOKEN);
 discordClient.once("ready", startMonitor);
